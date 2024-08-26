577390485951b218f21ac23241e6439d2a456402
AnnaKhairillina <98341165+AnnaKhairillina@users.noreply.github.com>
2024-08-26 12:01:27 +0500

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("my age:"+5 * 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
